4a0f0cc34b3fdf1d58275d9bc505f72f70295732
Niru Maheswaranathan <nirum@stanford.edu>
2015-09-10 18:45:20 -0700

    added tensor module

--- jetpack/__init__.py
+++ jetpack/__init__.py
@@ -9,11 +9,12 @@ Jetpack is a set of useful utility functions for python
 __version__ = '0.0.3'
 
 
-from . timepiece import *
-from . signals import *
-from . chart import *
-from . ionic import *
-from . capsules import *
+from .timepiece import *
+from .signals import *
+from .chart import *
+from .ionic import *
+from .capsules import *
+from .tensor import *
 
 __all__ = timepiece.__all__ + signals.__all__ + chart.__all__ + \
-    ionic.__all__ + capsules.__all__
+    ionic.__all__ + capsules.__all__ + tensor.__all__
