f2d94fc3f106d3d778c58c22381146419f645c66
Niru Maheswaranathan <nirum@stanford.edu>
2015-11-30 16:53:49 -0800

    Adds a simple HUD server to recieve and display charts

--- jetpack/__init__.py
+++ jetpack/__init__.py
@@ -6,13 +6,14 @@ Jetpack is a set of useful utility functions for python
 
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from .timepiece import *
 from .signals import *
 from .chart import *
 from .ionic import *
 from .capsules import *
+from .hud import *
 
 __all__ = timepiece.__all__ + signals.__all__ + chart.__all__ + \
     ionic.__all__ + capsules.__all__
---
+++ jetpack/hud.py
@@ -0,0 +1,80 @@
+"""
+Heads up display
+----------------
+
+"""
+import os
+import threading
+import tempfile
+import matplotlib.pyplot as plt
+from shutil import rmtree
+from functools import wraps
+from time import strftime
+
+VALID_FUNCTIONS = {'plot', 'imshow', 'contour', 'contourf', 'semilogx',
+                   'semilogy', 'loglog', 'hist', 'hist2d', 'bar', 'pcolor',
+                   'pcolormesh', 'scatter', 'fill', 'fill_between', 'errorbar'}
+__all__ = ['HUD']
+
+
+class HUD:
+
+    def __init__(self):
+
+        # Generate a temporary directory
+        self.path = tempfile.mkdtemp(prefix='hud_')
+
+        # start the server
+        self.start(port=8080)
+
+    def start(self, port):
+        self.server = Server(self.path, port=port)
+        self.server.start()
+
+    def _plot_wrapper(self, func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            plt.figure()
+            plt.ioff()
+            func(*args, **kwargs)
+            plt.draw()
+            filename = os.path.join(self.path, strftime('%b %d %Y %H_%M_%S.jpg'))
+            plt.savefig(filename, bbox_inches='tight')
+            plt.close()
+
+        return wrapper
+
+    def __iter__(self):
+        return iter(os.listdir(self.path))
+
+    def __dir__(self):
+        """
+        for autocompletion
+        """
+        return VALID_FUNCTIONS
+
+    def __del__(self):
+        rmtree(self.path)
+
+    def __getattr__(self, func):
+
+        if func not in VALID_FUNCTIONS:
+            raise ValueError('Function "' + func + '" is not a valid function')
+
+        return self._plot_wrapper(getattr(plt, func))
+
+class Server(threading.Thread):
+
+    def __init__(self, path, port=8000):
+        self.path = path
+        self.port = port
+        super().__init__()
+
+    def run(self):
+        """
+        Hack to get python to serve the given directory
+
+        """
+        cmd = 'cd {}; python3 -m http.server {}'.format(self.path, self.port)
+        os.system(cmd)
