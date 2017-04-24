69e5e46a6bdb7a9ee7e0c7e0ed3b9067fdad0fe0
Alex Williams <alex.h.willia@gmail.com>
2017-04-24 13:05:32 -0700

    cast to int

--- jetpack/chart/movie.py
+++ jetpack/chart/movie.py
@@ -16,7 +16,7 @@ def movieclip(makeframe, ax=None, **kwargs):
     # wrap makeframe function with mpltfig_to_npimage
     def _makeframe(t):
         print(t)
-        makeframe(t, ax=ax)
+        makeframe(int(t), ax=ax)
         return mplfig_to_npimage(plt.gcf())
 
     # return clip
