6ee69b2f304f8ab826b7c3011401930f12d61eef
Niru Nahesh <niru@hey.com>
2025-10-14 21:43:06 -0700

    Bump version to 0.6.6

--- src/jetplot/__init__.py
+++ src/jetplot/__init__.py
@@ -1,6 +1,6 @@
 """Jetplot is a set of useful utility functions for scientific python."""
 
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 
 from . import colors as c  # noqa: F401
 from .chart_utils import *
